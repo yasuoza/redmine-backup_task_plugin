e56f9035321e8e99f8af4ed72369e7de19cccf4d
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2014-03-20 18:04:46 +0900

    Update plugin info

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_backup_task do
   name 'Redmine Backup Task plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  author 'Yasuharu Ozaki'
+  description 'Rake task plugin for redmine data backup and restore'
+  version '0.1.0'
+  url 'https://github.com/yasuoza/redmine-backup_task_plugin'
+  author_url 'http://www.yasuoza.com'
 end
